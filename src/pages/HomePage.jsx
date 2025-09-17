36d8f7a534f96dcab62145ef2bed7414773e3970
promiselb <waedmansour1222006@gmail.com>
2025-09-17 03:31:48 +0300

    Initial Commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import Hero from '../components/Hero';
+import HomeCards from '../components/HomeCards.jsx';
+import JobsListings from '../components/JobsListings.jsx';
+import ViewAllJobs from '../components/ViewAllJobs.jsx';
+
+const HomePage = () => {
+  return (
+    <>
+        <Hero />
+        <HomeCards  />
+        <JobsListings isHome={true}/>
+        <ViewAllJobs />    
+    </>
+  )
+}
+
+export default HomePage
