9d1d01b90f9dc992742b565d1b8d6c46544962e0
promiselb <waedmansour1222006@gmail.com>
2025-10-09 15:19:09 +0300

    Completing the style for SignIn and SignUp pages, and some other little things

--- src/components/ViewAllJobs.jsx
+++ src/components/ViewAllJobs.jsx
@@ -5,9 +5,9 @@ const ViewAllJobs = () => {
     <section className="m-auto max-w-lg my-10 px-6">
       <Link
         to="/jobs"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</Link
-      >
+        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">
+          View All Jobs
+        </Link>
     </section>
   )
 }
